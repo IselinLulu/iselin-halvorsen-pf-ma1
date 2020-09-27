a4c0c682c18bdbc84d7cd51ac81dfeec0a59da63
IselinLulu <iselin.rose.halvorsen@hotmail.no>
2020-09-27 23:13:52 +0200

    Create script.js

---
+++ iselin-halvorsen-pf-ma1/script.js
@@ -0,0 +1,57 @@
+// Question 1
+
+// This is the answer for q.1:
+    var firstVar =  '';
+
+
+// Question 2
+
+// This is the answer for q.2:
+    var name = "Iselin";
+
+
+// Question 3
+
+// This is the answer for q.3:
+    var myAGe = 28 ;
+
+
+// Question 4
+
+// This is the answer for q.4:
+    var division = 20 / 5 ;
+
+
+// Question 5
+
+// This is the answer for q.5:
+    var animal = "frog";    
+    if(animal === "frog"){
+        console.log("This is a frog");
+    }
+
+
+// Question 6
+
+// This is the answer for q.6:
+    var orderHasShipped = true;
+
+
+// Question 7
+
+//  This is the answer for q.7:
+    var orderHasShipped = true;
+
+    if(orderHasShipped === true) {
+    console.log("Your order has been shipped.")
+    }
+
+
+// Question 8
+
+// This is the answer for q.8:
+    for(var count = 0; count <= 9; count = count + 1){
+    console.log(count);
+    }
+
+
